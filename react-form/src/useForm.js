7f21f04eea4a1524286b62ff8fdb2b5186b22b19
fridman32 <yehonatan.fridman@gmail.com>
2021-08-30 13:48:46 +0300

    .

--- react-form/src/useForm.js
+++ react-form/src/useForm.js
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 import validateInfo from "./validateInfo";
+import axios from "axios"
 import './Form.css'
 
 const useForm = (callback, validateInfo) => {
@@ -27,6 +28,15 @@ const useForm = (callback, validateInfo) => {
 
         setErrors(validateInfo(values));
         setIsSubmitting(true);
+
+        //sending the user data to a json file to simulate call to a server
+        const url = "https://jsonplaceholder.typicode.com/posts"
+        axios.post(url, values).then(response => {
+            console.log(response);
+        })
+            .catch(err => {
+                console.log(err);
+            })
     };
 
     useEffect(() => {
